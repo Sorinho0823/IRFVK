f8593d47f56c1435e6133e129b81d0c050114760
Sorinho0823 <adam.soros@stu.uni-corvinus.hu>
2023-12-08 16:00:22 +0100

    Toy abstraction

---
+++ Week6/Abstraction/Toy.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Week6.Abstraction
+{
+    public abstract class Toy :Label
+    {
+        public Toy()
+        {
+            AutoSize = false;
+            Height = 50;
+            Width = 50;
+            Paint += Toy_Paint;
+        }
+
+        private void Toy_Paint(object sender, PaintEventArgs e)
+        {
+            DrawImage(e.Graphics);
+        }
+
+        protected abstract void DrawImage(Graphics g);
+      
+        public virtual void MoveToy()
+        {
+            Left += +1;
+        }
+    }
+}
