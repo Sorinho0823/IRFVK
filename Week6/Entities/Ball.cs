32c6e3f478eb9bb93b815e40b606e9daf6e5f37a
Sorinho0823 <adam.soros@stu.uni-corvinus.hu>
2023-12-08 16:03:14 +0100

    new ball class

--- Week6/Entities/Ball.cs
+++ Week6/Entities/Ball.cs
@@ -5,32 +5,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Week6.Abstraction;
+
 
 namespace Week6.Entities
 {
-    public class Ball : Label
+    public class Ball : Toy
     {
-        public Ball()
-        {
-            AutoSize = false;
-            Height = 50;
-            Width = 50;
-            Paint += Ball_Paint;
-        }
+        
 
-        private void Ball_Paint(object sender, PaintEventArgs e)
-        {
-            DrawImage(e.Graphics);
-        }
-
-        protected void DrawImage(Graphics g)
+        protected  override void DrawImage(Graphics g)
         {
             g.FillEllipse(new SolidBrush(Color.Blue), 0, 0, Width, Height);
         }
 
-        public void MoveBall()
-        {
-            Left += +1;
-        }
+       
     }
 }
