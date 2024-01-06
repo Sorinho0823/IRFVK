39b50248e91c02e743c2a3f2b7d2ba341abeeec4
Sorinho0823 <adam.soros@stu.uni-corvinus.hu>
2024-01-06 19:04:44 +0100

    week7

---
+++ carfactory/Entities/CarFactory.cs
@@ -0,0 +1,17 @@
+﻿using carfactory.Abstraction;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace carfactory.Entities
+{
+    public class CarFactory : IToyFactory
+    {
+        public Abstraction.Toy CreateNew()
+        {
+            return new Car();
+        }
+    }
+}
