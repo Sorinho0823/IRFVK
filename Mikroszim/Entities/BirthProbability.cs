b8f9ae93e0e85be0b81a91f458f69c6ce17ab12c
Sorinho0823 <adam.soros@stu.uni-corvinus.hu>
2024-01-08 19:12:22 +0100

    osztalyok

---
+++ Mikroszim/Entities/BirthProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mikroszim.Entities
+{
+    public class BirthProbability
+    {
+                public int Age { get; set; }
+                public int NbrOfChildren { get; set; }
+                public double BProbability { get; set; }
+    }
+}
