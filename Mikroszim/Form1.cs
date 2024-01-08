d0d61f9365d2d7bcae69497fae79bb64b002b636
Sorinho0823 <adam.soros@stu.uni-corvinus.hu>
2024-01-08 20:36:21 +0100

    3 metodus

--- Mikroszim/Form1.cs
+++ Mikroszim/Form1.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Mikroszim.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +14,74 @@ namespace Mikroszim
 {
     public partial class Form1 : Form
     {
+        List<Person> Population = new List<Person>();
+        List<BirthProbability> BirthProbabilities = new List<BirthProbability>();
+        List<DeathProbability> DeathProbabilities = new List<DeathProbability>();
         public Form1()
         {
             InitializeComponent();
         }
+
+        public List<Person> GetPopulation(string csvpath)
+        {
+            List<Person> population = new List<Person>();
+
+            using (StreamReader sr = new StreamReader(csvpath, Encoding.Default))
+            {
+                while (!sr.EndOfStream)
+                {
+                    var line = sr.ReadLine().Split(';');
+                    population.Add(new Person()
+                    {
+                        BirthYear = int.Parse(line[0]),
+                        Gender = (Gender)Enum.Parse(typeof(Gender), line[1]),
+                        NbrOfChildren = int.Parse(line[2])
+                    });
+                }
+            }
+
+            return population;
+        }
+        public List<BirthProbability> GetBirthProbabilities(string csvpath)
+        {
+            List<BirthProbability> BirthProbabilities = new List<BirthProbability>();
+            using (StreamReader sr = new StreamReader(csvpath, Encoding.Default))
+            {
+                while (!sr.EndOfStream)
+                {
+
+                    var line = sr.ReadLine().Split(',');
+                    BirthProbabilities.Add(new BirthProbability()
+                    {
+                        Age = int.Parse(line[0]),
+                        NbrOfChildren = int.Parse(line[1]),
+                        BProbability = int.Parse(line[2])   //csv-ben csak 0 szerepel
+
+                    });
+                }
+                return BirthProbabilities;
+            }
+        }
+
+        public List<DeathProbability> GetDeathProbabilities(string csvpath)
+        {
+
+            List<DeathProbability> DeathProbabilities = new List<DeathProbability>();
+            using (StreamReader sr = new StreamReader(csvpath, Encoding.Default))
+            {
+                var line = sr.ReadLine().Split(',');
+                DeathProbabilities.Add(new DeathProbability()
+                {
+                    Gender= (Gender)int.Parse(line[0]),     //cast??
+                    Age = int.Parse(line[1]),
+                    DProbability= int.Parse(line[2])        //2 db nem 0 van a csvben
+
+
+                });
+            }
+            return DeathProbabilities;
+         }
+
+
     }
 }
