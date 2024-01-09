4b48472f0128a237ef4700d0aed78e6979f2b46c
Sorinho0823 <adam.soros@stu.uni-corvinus.hu>
2024-01-09 15:52:44 +0100

    Final version wth the winner

--- week10/Form1.Designer.cs
+++ week10/Form1.Designer.cs
@@ -30,6 +30,7 @@ namespace week10
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -41,11 +42,22 @@ namespace week10
             this.label1.TabIndex = 0;
             this.label1.Text = "1. generáció";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(685, 63);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 79);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -57,6 +69,7 @@ namespace week10
         #endregion
 
         private System.Windows.Forms.Label label1;
+        public System.Windows.Forms.Button button1;
     }
 }
 
--- week10/Form1.cs
+++ week10/Form1.cs
@@ -15,6 +15,9 @@ namespace week10
     {
         GameController gc = new GameController();
         GameArea ga;
+        Brain winnerBrain = null;
+        
+        
 
         int populationSize = 100;
         int nbrOfSteps = 10;
@@ -23,6 +26,7 @@ namespace week10
         public Form1()
         {
             InitializeComponent();
+            button1.Visible = false;
             ga = gc.ActivateDisplay();  //a pálya betoltese
             this.Controls.Add(ga);      //megjelenitese
             //gc.AddPlayer();
@@ -48,8 +52,43 @@ namespace week10
                              orderby p.GetFitness() descending
                              select p;
             var topPerformers = playerList.Take(populationSize / 2).ToList();
+            
+            var winners = from p in topPerformers
+                          where p.IsWinner
+                          select p;
+            if (winners.Count() > 0)
+            {
+                winnerBrain = winners.FirstOrDefault().Brain.Clone();
+                gc.GameOver -= Gc_GameOver;
+                button1.Visible = true;
+                return;
+            }
 
+            gc.ResetCurrentLevel();
+            foreach (var p in topPerformers)
+            {
+                var b = p.Brain.Clone();
+                if (generation % 3 == 0)
+                    gc.AddPlayer(b.ExpandBrain(nbrOfStepsIncrement));
+                else
+                    gc.AddPlayer(b);
 
+                if (generation % 3 == 0)
+                    gc.AddPlayer(b.Mutate().ExpandBrain(nbrOfStepsIncrement));
+                else
+                    gc.AddPlayer(b.Mutate());
+            }
+            gc.Start();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            gc.ResetCurrentLevel();
+            gc.AddPlayer(winnerBrain.Clone());
+            gc.AddPlayer();
+            ga.Focus();
+            gc.Start(true);
         }
     }
 }
