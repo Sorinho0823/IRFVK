1b66390d811c04bc110b8230e5b30ff59f9d809b
Sorinho0823 <adam.soros@stu.uni-corvinus.hu>
2024-01-09 14:25:58 +0100

    vegso

--- Mikroszim/Form1.Designer.cs
+++ Mikroszim/Form1.Designer.cs
@@ -29,36 +29,94 @@ namespace Mikroszim
         /// </summary>
         private void InitializeComponent()
         {
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonB = new System.Windows.Forms.Button();
+            this.buttonStart = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // dataGridView1
+            // label1
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(168, 107);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 49;
-            this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(445, 243);
-            this.dataGridView1.TabIndex = 0;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Záró év";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(200, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "népesség fájl";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.richTextBox1.Location = new System.Drawing.Point(30, 78);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Horizontal;
+            this.richTextBox1.Size = new System.Drawing.Size(739, 369);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(308, 43);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(254, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // buttonB
+            // 
+            this.buttonB.Location = new System.Drawing.Point(589, 40);
+            this.buttonB.Name = "buttonB";
+            this.buttonB.Size = new System.Drawing.Size(73, 23);
+            this.buttonB.TabIndex = 4;
+            this.buttonB.Text = "Browse";
+            this.buttonB.UseVisualStyleBackColor = true;
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(694, 40);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(72, 23);
+            this.buttonStart.TabIndex = 5;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.dataGridView1);
+            this.ClientSize = new System.Drawing.Size(793, 479);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.buttonB);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonB;
+        private System.Windows.Forms.Button buttonStart;
     }
 }
 
--- Mikroszim/Form1.cs
+++ Mikroszim/Form1.cs
@@ -25,47 +25,19 @@ namespace Mikroszim
             BirthProbabilities = GetBirthProbabilities(@"C:\Temp\születés.csv");
             DeathProbabilities = GetDeathProbabilities(@"C:\Temp\halál.csv");
 
-           //szimuláció váza kezdete
+            //szimuláció váza kezdete
+            TeljesSyimulacio();
+
+        }
+
+        private void TeljesSyimulacio()
+        {
             for (int year = 2005; year <= 2024; year++) //evek
             {
-                
+
                 for (int i = 0; i < Population.Count; i++)      //osszes személy
                 {
-                    private void SimStep(int year, Person person)
-                    {
-                        //Ha halott akkor kihagyjuk, ugrunk a ciklus következő lépésére
-                        if (!person.IsAlive) return;
-
-                        // Letároljuk az életkort, hogy ne kelljen mindenhol újraszámolni
-                        byte age = (byte)(year - person.BirthYear);
-
-                        // Halál kezelése
-                        // Halálozási valószínűség kikeresése
-                        double pDeath = (from x in DeathProbabilities
-                                         where x.Gender == person.Gender && x.Age == age
-                                         select x.P).FirstOrDefault();
-                        // Meghal a személy?
-                        if (rng.NextDouble() <= pDeath)
-                            person.IsAlive = false;
-
-                        //Születés kezelése - csak az élő nők szülnek
-                        if (person.IsAlive && person.Gender == Gender.Female)
-                        {
-                            //Szülési valószínűség kikeresése
-                            double pBirth = (from x in BirthProbabilities
-                                             where x.Age == age
-                                             select x.P).FirstOrDefault();
-                            //Születik gyermek?
-                            if (rng.NextDouble() <= pBirth)
-                            {
-                                Person újszülött = new Person();
-                                újszülött.BirthYear = year;
-                                újszülött.NbrOfChildren = 0;
-                                újszülött.Gender = (Gender)(rng.Next(1, 3));
-                                Population.Add(újszülött);
-                            }
-                        }
-                    }
+                    SimStep(year, i);
                 }
 
                 int nbrOfMales = (from x in Population
@@ -77,7 +49,6 @@ namespace Mikroszim
                 Console.WriteLine(
                     string.Format("Év:{0} Fiúk:{1} Lányok:{2}", year, nbrOfMales, nbrOfFemales));
             }
-
         }
 
         public List<Person> GetPopulation(string csvpath)
@@ -139,6 +110,40 @@ namespace Mikroszim
             }
             return DeathProbabilities;
          }
+
+        private void SimStep(int year, Person person)
+        {
+
+            if (!person.IsAlive) return;        //él-e
+
+            byte age = (byte)(year - person.BirthYear);     //életkor
+
+            
+            double pDeath = (from x in DeathProbabilities           //halalozás valoszinusege
+                             where x.Gender == person.Gender && x.Age == age
+                             select x.DProbability).FirstOrDefault();
+            
+            if (rng.NextDouble() <= pDeath)     //meghal 
+                person.IsAlive = false;
+
+            //Születés kezelése - csak az élő nők szülnek
+            if (person.IsAlive && person.Gender == Gender.Female)
+            {
+                
+                double pBirth = (from x in BirthProbabilities
+                                 where x.Age == age
+                                 select x.BProbability).FirstOrDefault();
+                
+                if (rng.NextDouble() <= pBirth)             //ha szuletik, és adatokat kap
+                {
+                    Person újszülött = new Person();
+                    újszülött.BirthYear = year;
+                    újszülött.NbrOfChildren = 0;
+                    újszülött.Gender = (Gender)(rng.Next(1, 3));
+                    Population.Add(újszülött);
+                }
+            }
+        }
 
 
     }
